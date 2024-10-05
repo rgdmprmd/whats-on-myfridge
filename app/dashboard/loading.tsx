336c768386628ca55eda69ee21f759625d540c74
Rangga <ranggadpermadi@gmail.com>
2024-10-05 18:02:00 +0800

    feat: add a loading page

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadingDashboard = () => {
+	return (
+		<div className="flex flex-col items-center justify-center h-[calc(100vh-100px)] border">
+			<h1>Relax, we are working on it.</h1>
+		</div>
+	);
+};
+
+export default LoadingDashboard;
